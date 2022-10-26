9ab676d40ade96207ec7e55c631eb5634b8e9b64
cwpersonrennell <94017839+cwpersonrennell@users.noreply.github.com>
2022-10-25 17:10:08 -0700

    Update copyToClipboard.js

--- copyToClipboard.js
+++ copyToClipboard.js
@@ -1,10 +1,12 @@
 function copyToClipboard(el){
   let text = el.innerHTML;
   let input =document.createElement("input");
+  text.replaceAll(`&gt;`,">");
+  text.replaceAll(`&lt;`,"<");
   input.value = text;
   document.body.append(input);
   input.select();
-  console.log(input);
+  
   document.execCommand("copy");
   document.body.removeChild(input);
 }
