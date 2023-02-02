69ad507f525e0a1837b156557a2e3a964365b667
cwpersonrennell <94017839+cwpersonrennell@users.noreply.github.com>
2023-02-02 16:42:44 -0700

    Update hide.js

--- hide.js
+++ hide.js
@@ -1,18 +1,21 @@
 var show_solution = false;
-var button = document.getElementById('toggle_solution');
-button.addEventListener("click",function(){
-  var el = document.getElementById('solution');
-  try{
-    if(show_solution){
 
-      button.innerHTML = "Show Solution";
-      el.classList.add('hide');
-      el.classList.remove('show');
-    }else{
-      button.innerHTML = "Hide Solution";
-      el.classList.add('show');
-      el.classList.remove('hide');
-    }
-    show_solution = !show_solution;
-  }catch(){}//pass
-});
+var button = document.getElementById('toggle_solution');
+try{
+  button.addEventListener("click",function(){
+    var el = document.getElementById('solution');
+    try{
+      if(show_solution){
+
+        button.innerHTML = "Show Solution";
+        el.classList.add('hide');
+        el.classList.remove('show');
+      }else{
+        button.innerHTML = "Hide Solution";
+        el.classList.add('show');
+        el.classList.remove('hide');
+      }
+      show_solution = !show_solution;
+    }catch(){}//pass
+  });
+}catch(){//pass}
