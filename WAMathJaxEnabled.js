4cd949f9062ded4ceba86113aa2af25c1fa7db7d
asilio <cpersonrennell@gmail.com>
2023-04-06 09:27:09 -0700

    Update WAMathJaxEnabled.js

--- WAMathJaxEnabled.js
+++ WAMathJaxEnabled.js
@@ -1,28 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
-        getCalculators();
         const iframe = document.createElement('iframe');
         iframe.src="about:blank";
-        const question = document.getElementById("WAMathJax");
-        iframe.style=question.style;
+        const usercontent = document.querySelector("#WAMathJax").content.cloneNode(true);
+        iframe.style=usercontent.style;
         iframe.frameBorder=0;
         let content = document.createElement('div');
-        content.innerHTML=question.innerHTML;
+        content.innerHTML=usercontent.innerHTML;
         let source = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
 
         iframe.width=800;
 
-        iframe.height=question.clientHeight*1.5;
-        question.innerHTML="";
-        question.append(iframe);
+        iframe.height=usercontent.clientHeight*1.5;
+        let target = document.getElementById("target");
+        target.append(iframe);
+        
         const doc = iframe.contentDocument;
         
-        let mjax = doc.createElement("script");
-        mjax.src='https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
-        mjax.async=true;
-        content.append(mjax);
+        let scripts = [
+                'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js',
+                "https://cwpersonrennell.github.io/DesmosAddons/DesmosAddons.js",
+                "https://cwpersonrennell.github.io/DesmosAddons/hide.js",
+                ];
+        
+        for(let i = 0; i<scripts.length;i++){
+                let script = doc.createElement("script");
+                script.src = scripts[i];
+                script.async = true;
+                content.append(script);
+        }
+        
         const body = doc.createElement('body');
         body.append(content);
         doc.body=body;
-       
-        
-}
+          
+});
