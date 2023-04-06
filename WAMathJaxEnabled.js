fb55151cb46e46ed1c8b6cfdcef9b3255c6058e8
asilio <94017839+cwpersonrennell@users.noreply.github.com>
2023-04-06 10:00:48 -0700

    Updated

--- WAMathJaxEnabled.js
+++ WAMathJaxEnabled.js
@@ -2,20 +2,14 @@ document.addEventListener("DOMContentLoaded", () => {
         const iframe = document.createElement('iframe');
         iframe.src="about:blank";
         const usercontent = document.querySelector("#WAMathJax").content.cloneNode(true);
-        iframe.style=usercontent.style;
         iframe.frameBorder=0;
-        let content = document.createElement('div');
-        content.innerHTML=usercontent.innerHTML;
-        let source = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
-
         iframe.width=800;
 
-        iframe.height=usercontent.clientHeight*1.5;
         let target = document.getElementById("target");
         target.append(iframe);
         
         const doc = iframe.contentDocument;
-        
+        const body = doc.createElement('body');
         let scripts = [
                 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js',
                 "https://cwpersonrennell.github.io/DesmosAddons/DesmosAddons.js",
@@ -26,11 +20,8 @@ document.addEventListener("DOMContentLoaded", () => {
                 let script = doc.createElement("script");
                 script.src = scripts[i];
                 script.async = true;
-                content.append(script);
+                body.append(script);
         }
-        
-        const body = doc.createElement('body');
-        body.append(content);
-        doc.body=body;
-          
+        body.append(usercontent);
+        doc.body=body;      
 });
