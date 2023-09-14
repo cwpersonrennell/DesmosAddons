940e4cb195a868f4c1cd3ab82df9272e1b6306ee
cwpersonrennell <94017839+cwpersonrennell@users.noreply.github.com>
2023-09-13 22:12:21 -0700

    Update WAMathJaxEnabled.js

--- WAMathJaxEnabled.js
+++ WAMathJaxEnabled.js
@@ -1,49 +1,72 @@
-document.addEventListener("DOMContentLoaded", () => {
-        console.log("WAMathJaxEnabled.js is loaded and running");
-        const iframe = document.createElement('iframe');
-        iframe.style="min-width:800px;max-width:800px;";
-        iframe.src="about:blank";
-        const usercontent = document.querySelector("#WAMathJax").content.cloneNode(true);
-        iframe.frameBorder=0;
-        iframe.width=800;
-        function bootstrap(){
-                //console.log("bootstrap");
-                try{
-                        //console.log("iFrame load calculators");
-                        iframe.contentWindow.Desmos.GraphingCalculator();
-                        iframe.contentWindow.getCalculators();
-                        //console.log(iframe.contentWindow.document.body);
-                        iframe.height = iframe.contentWindow.document.body.scrollHeight+30;
-                }catch(err){
-                        //console.log(err);
-                        //console.log(iframe.contentWindow);
-                        setTimeout(bootstrap,1000);
+var WAMathJax = WAMathJax || {};
+if(WAMathJax.run_complete){}else{
+        WAMathJax.iframeFactory=function(){
+                function bootstrap(){
+                        //console.log("bootstrap");
+                        try{
+                                //console.log("iFrame load calculators");
+                                this.contentWindow.Desmos.GraphingCalculator();
+                                this.contentWindow.getCalculators();
+                                //console.log(iframe.contentWindow.document.body);
+                                this.height = this.contentWindow.document.body.scrollHeight+30;
+                        }catch(err){
+                                //console.log(err);
+                                //console.log(iframe.contentWindow);
+                                setTimeout(bootstrap,1000);
+                        }
+                        
                 }
-                
+                let iframe = document.createElement('iframe');
+                iframe.style="min-width:800px;max-width:800px;";
+                iframe.src="about:blank";
+                iframe.frameBorder=0;
+                iframe.width=800;
+                iframe.addEventListener('load',bootstrap.bind(iframe));
+                return iframe;
         }
-        
-        iframe.addEventListener('load',bootstrap);
-        let target = document.getElementById("target");
-        target.append(iframe);
-        //document.body.append(iframe);
-        
-        const doc = iframe.contentDocument;
-        const body = doc.createElement('body');
-        let scripts = [
-"https://www.desmos.com/api/v1.7/calculator.js?apiKey=dcb31709b452b1cf9dc26972add0fda6",
-                'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js',
-                "https://cwpersonrennell.github.io/DesmosAddons/DesmosAddons.js",
-                "https://cwpersonrennell.github.io/DesmosAddons/hide.js"
-                ];
-        
-        for(let i = 0; i<scripts.length;i++){
-                let script = doc.createElement("script");
-                script.src = scripts[i];
-                script.async = true;
-                body.append(script);
+
+        WAMathJax.addScriptsToBody=function(scripts, body, doc){
+                for(let i = 0; i<scripts.length;i++){
+                        let script = doc.createElement("script");
+                        script.src = scripts[i];
+                        script.async = true;
+                        body.append(script);
+                }
         }
-        body.append(usercontent);
-        body.style.setProperty('max-width','800px');
-        body.style.setProperty('overflow-x','clip');
-        doc.body=body;      
-});
+
+        WAMathJax.loadUserContent=function(template){
+                const usercontent = template.content.cloneNode(true);
+                const iframe = WAMathJax.iframeFactory();
+                const target = template.parentElement;
+                target.append(iframe);
+                const doc = iframe.contentDocument;
+                const body = doc.createElement('body');
+                let scripts = [
+                        "https://www.desmos.com/api/v1.7/calculator.js?apiKey=dcb31709b452b1cf9dc26972add0fda6",
+                        'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js',
+                        "https://cwpersonrennell.github.io/DesmosAddons/DesmosAddons.js",
+                        "https://cwpersonrennell.github.io/DesmosAddons/hide.js"
+                        ];
+                WAMathJax.addScriptsToBody(scripts,body,doc);
+                body.append(usercontent);
+                body.style.setProperty('max-width','800px');
+                body.style.setProperty('overflow-x','clip');
+                doc.body=body;  
+        }
+
+        WAMathJax.run_complete=true;
+        WAMathJax.DOMComplete = false;
+
+        
+        document.addEventListener("DOMContentLoaded", () => {
+                if(WAMathJax.DOMComplete) return;
+                WAMathJax.DOMComplete = true;
+
+                let templates = document.querySelectorAll("#WAMathJax");
+                for(template of templates){
+                        WAMathJax.loadUserContent(template);
+                }
+        });
+}
+
+
