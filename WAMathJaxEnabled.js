d82c9e9392a139dc40d9699658aedb6c6a045fb3
cwpersonrennell <94017839+cwpersonrennell@users.noreply.github.com>
2024-10-14 20:47:01 -0700

    added additional link to included features

--- WAMathJaxEnabled.js
+++ WAMathJaxEnabled.js
@@ -40,7 +40,8 @@ if(WAMathJax.run_complete){}else{
                         "https://www.desmos.com/api/v1.7/calculator.js?apiKey=dcb31709b452b1cf9dc26972add0fda6",
                         'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js',
                         "https://cwpersonrennell.github.io/DesmosAddons/DesmosAddons.js",
-                        "https://cwpersonrennell.github.io/DesmosAddons/hide.js"
+                        "https://cwpersonrennell.github.io/DesmosAddons/hide.js",
+                        "https://cwpersonrennell.github.io/Math/WA.js"
                         ];
                 WAMathJax.addScriptsToBody(scripts,body,doc);
                 body.append(usercontent);
